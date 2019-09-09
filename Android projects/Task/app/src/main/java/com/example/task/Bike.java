7e4bde4f9599dc3fb63eca0503648bfc8899db63
Parvati V M <parvatimathapati669@gmail.com>
2019-09-09 17:05:51 +0530

    constructor

--- Android projects/Task/app/src/main/java/com/example/task/Bike.java
+++ Android projects/Task/app/src/main/java/com/example/task/Bike.java
@@ -1,8 +1,27 @@
 package com.example.task;
 
-public class Bike {
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Bike extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
     Bike()
     {
         System.out.println("bike is created");
     }
-}
+
+    public void car(Context applicationContext) {
+        Toast.makeText(applicationContext, "Car is created", Toast.LENGTH_SHORT).show();
+
+    }
+    }
--- Android projects/Task/app/src/main/java/com/example/task/MainActivity.java
+++ Android projects/Task/app/src/main/java/com/example/task/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bike b1=new Bike();
+                b1.car(getApplicationContext());
+            }
+        });
 
-        Bike b1=new Bike();
-              
+
+
+
+            }
+
+
 
     }
-}
+
